7ef1939182bd24d872a888e97c05e36b0a8b3b6f
takagotch <dyaccb@gmail.com>
2018-01-17 08:17:39 +0900

    ConsoleLogBuildWebpackPlugin.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack           = require('webpack');
+const path              = require('path');
+
 module.exports = {
-  entry: './path/to/my/entry/file.js'
+  entry: './path/to/my/entry/file.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'my-first-webpack.bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.txt$/, use: 'raw-loader' }
+    ]
+  }
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new HtmlWebpackPlugin({template: './src/index.html'})
+  ]
 };
+
+module.exports = config;
+
 
